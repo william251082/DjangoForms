2f971b8391ffa8f8cf43becdd578c0daee417c9a
williamdelrosario <williamdelrosario@yahoo.com>
2017-10-18 13:40:56 +0200

    Email Verification

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,20 +1,17 @@
 from django import forms
 from django.core import validators
 
-def check_for_z(value):
-    if value[0].lower() != 'z':
-        raise forms.ValidationError("NAME NEEDS TO START WITH Z")
-
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label="Ente you email agan:")
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,
-                                 widget=forms.HiddenInput,
-                                 validators=[validators.MaxLengthValidator(0)])
 
-'''def clean_botcatcher(self):
-    botcatcher = self.cleaned_data['botcatcher']
-    if len(botcatcher) > 0:
-        raise forms.ValidationError("GOTCHA BOT!")
-    return botcatcher'''
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")
